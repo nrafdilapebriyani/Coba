0aa254c8396ddbd1a0a7f497856748aa7d07a319
nrafdilapebriyani <nrafdilafebri29@gmailcom>
2021-04-11 01:01:44 +0700

    menu_popup

---
+++ app/src/main/java/com/example/coba/ClassNama.java
@@ -0,0 +1,15 @@
+package com.example.coba;
+
+public class ClassNama {
+        private String  Nama;
+
+        public  ClassNama(String Nama)
+        {
+            this.Nama = Nama;
+
+        }
+        public String  getNama() {
+            return this.Nama;
+        }
+
+}
